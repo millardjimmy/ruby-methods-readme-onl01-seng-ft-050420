e7e6e4f31ec954017afffba063fcd6dc9933bbff
Jimmy Millard <millardjimmy@gmail.com>
2020-05-04 17:07:25 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,6 +1,6 @@
-# Use this space to code along with the readme
-
-phrase = "Hello World!"
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
 puts phrase
 puts phrase
 puts phrase
@@ -10,4 +10,4 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
-puts phrase
+end
